e3f7728510a768fd50bc4fa06177fce7a5950e22
Azhaan Muhammad Pankaj <azhaanmd.personal@gmail.com>
2024-06-21 20:17:04 +0600

    Added POM and Login

--- tests/test_basic_flow_e2e.py
+++ tests/test_basic_flow_e2e.py
@@ -1,9 +1,13 @@
+from time import sleep
+
 import pytest
-from selenium.webdriver.common.by import By
 from utilities.BaseClass import BaseClass
+from pageObjects.loginPage import LoginPage
 
 class TestBasicFlow(BaseClass):
     def test_basic_flow(self):
-        self.driver.find_element(By.CSS_SELECTOR, "#user-name").send_keys("standard_user")
-        self.driver.find_element(By.CSS_SELECTOR, "#password").send_keys("secret_sauce")
-        self.driver.find_element(By.CSS_SELECTOR, "#login-button").click()
+        loginPage = LoginPage(self.driver)
+        loginPage.fillLoginInfo("standard_user", "secret_sauce")
+        loginPage.getLoginButton().click()
+
+        sleep(3)
